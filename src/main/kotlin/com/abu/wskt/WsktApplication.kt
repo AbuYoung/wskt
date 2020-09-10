1a100f0bbf04ed83108ddf569c4aaef7026a726e
AbuYoung <abuyoung@outlook.com>
2020-09-10 17:18:50 +0800

    git init

---
+++ src/main/kotlin/com/abu/wskt/WsktApplication.kt
@@ -0,0 +1,11 @@
+package com.abu.wskt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WsktApplication
+
+fun main(args: Array<String>) {
+    runApplication<WsktApplication>(*args)
+}
---
+++ src/test/kotlin/com/abu/wskt/WsktApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abu.wskt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WsktApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
